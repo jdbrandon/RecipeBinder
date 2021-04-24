d75e12b41d1001dcb5758170e30303e3c4f90e3d
Jeff Brandon <jeff@jeffbrandon.me>
2021-04-24 16:56:54 -0700

    Adding qr dependency and creating the new fragment and layout

---
+++ RecipeBinderApp/src/main/java/com/jeffbrandon/recipebinder/fragments/ShareRecipeFragment.kt
@@ -0,0 +1,14 @@
+package com.jeffbrandon.recipebinder.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.jeffbrandon.recipebinder.R
+
+class ShareRecipeFragment: Fragment(R.layout.fragment_share_recipe) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
--- RecipeBinderApp/src/main/java/com/jeffbrandon/recipebinder/fragments/ViewFragmentPagerAdapter.kt
+++ RecipeBinderApp/src/main/java/com/jeffbrandon/recipebinder/fragments/ViewFragmentPagerAdapter.kt
@@ -4,6 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 
 class ViewFragmentPagerAdapter(activity: FragmentActivity) : FragmentPagerAdapter(activity) {
-    override val fragments: List<Lazy<Fragment>> =
-        listOf(lazy { IngredientFragment() }, lazy { InstructionFragment() })
+    override val fragments: List<Lazy<Fragment>> = listOf(lazy { IngredientFragment() },
+                                                          lazy { InstructionFragment() },
+                                                          lazy { ShareRecipeFragment() })
 }
