14801aca0289d50b8e8367bbb22c628f30121548
Jeff Brandon <jeff@jeffbrandon.me>
2021-04-25 22:39:52 -0700

    update room version, see if delete by id works in ci?

--- RecipeBinderApp/src/main/java/com/jeffbrandon/recipebinder/room/RecipeMenuDataSource.kt
+++ RecipeBinderApp/src/main/java/com/jeffbrandon/recipebinder/room/RecipeMenuDataSource.kt
@@ -1,6 +1,7 @@
 package com.jeffbrandon.recipebinder.room
 
 import androidx.lifecycle.LiveData
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 
@@ -9,7 +10,9 @@ interface RecipeMenuDataSource {
     @Insert
     fun insertRecipe(recipe: RecipeData): Long
 
-    @Query("DELETE FROM RecipeData WHERE id = :recipeId")
+    // TODO delete or revert
+    //    @Query("DELETE FROM RecipeData WHERE id = :recipeId")
+    @Delete(entity = RecipeData::class)
     fun deleteRecipe(recipeId: Long?): Int
 
     @Query("SELECT * FROM RecipeData WHERE name like :filter")
