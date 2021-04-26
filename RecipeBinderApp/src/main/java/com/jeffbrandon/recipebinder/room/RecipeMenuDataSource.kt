7038524ba23001a9bab470955554d1fb8e745eca
Jeff Brandon <jeff@jeffbrandon.me>
2021-04-25 22:41:25 -0700

    keep query

--- RecipeBinderApp/src/main/java/com/jeffbrandon/recipebinder/room/RecipeMenuDataSource.kt
+++ RecipeBinderApp/src/main/java/com/jeffbrandon/recipebinder/room/RecipeMenuDataSource.kt
@@ -1,7 +1,6 @@
 package com.jeffbrandon.recipebinder.room
 
 import androidx.lifecycle.LiveData
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 
@@ -10,9 +9,7 @@ interface RecipeMenuDataSource {
     @Insert
     fun insertRecipe(recipe: RecipeData): Long
 
-    // TODO delete or revert
-    //    @Query("DELETE FROM RecipeData WHERE id = :recipeId")
-    @Delete(entity = RecipeData::class)
+    @Query("DELETE FROM RecipeData WHERE id = :recipeId")
     fun deleteRecipe(recipeId: Long?): Int
 
     @Query("SELECT * FROM RecipeData WHERE name like :filter")
