144eb42385acb134660ed103e84ac234f02541a5
Jeff Brandon <jeff@jeffbrandon.me>
2021-05-16 13:45:55 -0700

    Refactor converters to be kotlin objects add proguard keep rule to preserve names and line numbers

--- RecipeBinderApp/src/main/java/com/jeffbrandon/recipebinder/room/RecipeTagConverter.kt
+++ RecipeBinderApp/src/main/java/com/jeffbrandon/recipebinder/room/RecipeTagConverter.kt
@@ -3,27 +3,24 @@ package com.jeffbrandon.recipebinder.room
 import androidx.room.TypeConverter
 import com.jeffbrandon.recipebinder.enums.RecipeTag
 
-class RecipeTagConverter private constructor() {
-
-    companion object {
-        @TypeConverter
-        @JvmStatic
-        fun toInt(tags: Set<RecipeTag>): Int {
-            var res = 0
-            for (tag in tags) {
-                res = res or (1 shl RecipeTag.values.indexOf(tag))
-            }
-            return res
+object RecipeTagConverter {
+    @TypeConverter
+    @JvmStatic
+    fun toInt(tags: Set<RecipeTag>): Int {
+        var res = 0
+        for (tag in tags) {
+            res = res or (1 shl RecipeTag.values.indexOf(tag))
         }
+        return res
+    }
 
-        @TypeConverter
-        @JvmStatic
-        fun toListRecipeTag(tags: Int): Set<RecipeTag> {
-            val res = mutableSetOf<RecipeTag>()
-            for (i in RecipeTag.values.indices) {
-                if ((1 shl i) and tags != 0) res.add(RecipeTag.values[i])
-            }
-            return res
+    @TypeConverter
+    @JvmStatic
+    fun toListRecipeTag(tags: Int): Set<RecipeTag> {
+        val res = mutableSetOf<RecipeTag>()
+        for (i in RecipeTag.values.indices) {
+            if ((1 shl i) and tags != 0) res.add(RecipeTag.values[i])
         }
+        return res
     }
 }
