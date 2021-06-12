a208b8aaa68e2cfc720767fb9d74cb25f43cbad6
Jeff Brandon <jdbrando@andrew.cmu.edu>
2021-06-12 14:26:47 -0700

    convert EditRecipeViewModel.kt and RecipeViewModel.kt to flow and unit-test fixup

--- RecipeBinderApp/src/test/java/com/jeffbrandon/recipebinder/viewmodel/RecipeViewModelTest.kt
+++ RecipeBinderApp/src/test/java/com/jeffbrandon/recipebinder/viewmodel/RecipeViewModelTest.kt
@@ -3,21 +3,15 @@ package com.jeffbrandon.recipebinder.viewmodel
 import android.content.Context
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.liveData
 import com.jeffbrandon.recipebinder.R
+import com.jeffbrandon.recipebinder.dagger.IDispatchers
 import com.jeffbrandon.recipebinder.room.RecipeDataSource
+import com.jeffbrandon.recipebinder.testutils.MainCoroutineRule
 import com.jeffbrandon.recipebinder.testutils.TestRecipeData
 import com.jeffbrandon.recipebinder.testutils.getOrAwaitValue
 import junit.framework.TestCase.assertEquals
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.TestCoroutineScope
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.setMain
-import org.junit.After
+import kotlinx.coroutines.flow.flow
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -35,53 +29,55 @@ class RecipeViewModelTest {
         private const val EXTRA_VAL = 1L
     }
 
-    @get:Rule val instantExecutorRule = InstantTaskExecutorRule()
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
 
-    @Mock private lateinit var dataSource: RecipeDataSource
-    @Mock private lateinit var context: Context
+    @get:Rule
+    val rule = MainCoroutineRule()
+
+    @Mock
+    private lateinit var dataSource: RecipeDataSource
+
+    @Mock
+    private lateinit var context: Context
     private lateinit var underTest: RecipeViewModel
-
-    private val dispatcher = TestCoroutineDispatcher()
-    private val scope = TestCoroutineScope(dispatcher)
 
     @Before
     fun setUp() {
         MockitoAnnotations.openMocks(this)
-        Dispatchers.setMain(dispatcher)
         whenever(context.getString(R.string.extra_recipe_id)).thenReturn(KEY_EXTRA_ID)
-        whenever(dataSource.fetchRecipe(eq(EXTRA_VAL))).thenReturn(liveData { emit(TestRecipeData.RECIPE_1) })
-        underTest = RecipeViewModel({ dataSource }, SavedStateHandle(mapOf(KEY_EXTRA_ID to EXTRA_VAL)), context)
-    }
-
-    @After
-    fun tearDown() {
-        scope.advanceUntilIdle()
-        dispatcher.cleanupTestCoroutines()
-        Dispatchers.resetMain()
-    }
-
-    @Test
-    fun `test get recipe`(): Unit = runBlocking {
-        scope.launch {
-            val recipe = underTest.getRecipe().getOrAwaitValue()
-            assertEquals(TestRecipeData.RECIPE_1, recipe)
-            verify(dataSource).fetchRecipe(eq(EXTRA_VAL))
-        }
+        whenever(dataSource.fetchRecipe(eq(EXTRA_VAL))).thenReturn(flow { emit(TestRecipeData.RECIPE_1) })
+        underTest = RecipeViewModel(
+            { dataSource },
+            SavedStateHandle(mapOf(KEY_EXTRA_ID to EXTRA_VAL)),
+            context,
+            {
+                object :
+                    IDispatchers {
+                    override val default = rule.dispatcher
+                    override val io = rule.dispatcher
+                    override val main = rule.dispatcher
+                }
+            },
+        )
     }
 
     @Test
-    fun `test get ingredients`(): Unit = runBlocking {
-        scope.launch {
-            val ingredients = underTest.getIngredients().getOrAwaitValue()
-            assertEquals(TestRecipeData.RECIPE_1.ingredients, ingredients)
-        }
+    fun `test get recipe`() = rule.runBlockingTest {
+        val recipe = underTest.getRecipe().getOrAwaitValue()
+        assertEquals(TestRecipeData.RECIPE_1, recipe)
+        verify(dataSource).fetchRecipe(eq(EXTRA_VAL))
     }
 
     @Test
-    fun `test get instructions`(): Unit = runBlocking {
-        scope.launch {
-            val instructions = underTest.getInstructions().getOrAwaitValue()
-            assertEquals(TestRecipeData.RECIPE_1.instructions, instructions)
-        }
+    fun `test get ingredients`() = rule.runBlockingTest {
+        val ingredients = underTest.getIngredients().getOrAwaitValue()
+        assertEquals(TestRecipeData.RECIPE_1.ingredients, ingredients)
+    }
+
+    @Test
+    fun `test get instructions`() = rule.runBlockingTest {
+        val instructions = underTest.getInstructions().getOrAwaitValue()
+        assertEquals(TestRecipeData.RECIPE_1.instructions, instructions)
     }
 }
